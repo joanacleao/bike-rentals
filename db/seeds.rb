d893b3c2babe3aa86e427d31432066dfd7e43238
joanaleao <joanacastroleao@gmail.com>
2018-02-28 15:31:45 +0000

    added more 6 bikes

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,5 +32,23 @@ bike_5.save!
 bike_6 = Bike.new(brand: "Batavus", spec: "mountain", description:"Perfect for downhill", city: "Lisbon", address:"Rua Alexandre Herculano 4, Lisboa", price: 55, photo: "http://res.cloudinary.com/dywzzd0as/image/upload/v1519822179/trek7k.jpg")
 bike_6.user = user_2
 bike_6.save!
+bike_7 = Bike.new(brand: "Schimano", spec: "bmx", description:"Good bike", city: "Lisbon", address:"Av Liberdade 18, Lisboa", price: 50, photo: "http://res.cloudinary.com/dywzzd0as/image/upload/v1519817070/Z6605.jpg")
+bike_7.user = user_1
+bike_7.save!
+bike_8 = Bike.new(brand: "Pinarello", spec: "bmx", description:"Bad bike", city: "Lisbon", address:"Rua Nova do Carvalho, 20, Lisboa", price: 60, photo: "http://res.cloudinary.com/dywzzd0as/image/upload/v1519817064/FIT_BIKE_CO_TRL_TRANS_GREEN_2018.jpg")
+bike_8.user = user_2
+bike_8.save!
+bike_9 = Bike.new(brand: "Schimano", spec: "mountain", description:"Best bike", city: "Lisbon", address:"Rua Moeda 7, Lisboa", price: 50, user_id: 2, photo: "http://res.cloudinary.com/dywzzd0as/image/upload/v1519822176/maxresdsdsefault.jpg")
+bike_9.user = user_3
+bike_9.save!
+bike_10 = Bike.new(brand: "BMC", spec: "road", description:"Fast bike", city: "Lisbon", address:"Av Marginal 30, Lisboa", price: 40, photo: "http://res.cloudinary.com/dywzzd0as/image/upload/v1519822391/whyte-wessex-01-1505906320879-rh41p87ozlvv-1507723597398-bz1f98h8jsql-630-354.jpg")
+bike_10.user = user_3
+bike_10.save!
+bike_11 = Bike.new(brand: "TREK", spec: "road", description:"Beautiful bike", city: "Lisbon", address:"Av Republica 57, Lisboa", price: 65, photo: "http://res.cloudinary.com/dywzzd0as/image/upload/v1519822396/reid-granite-gravelbike-17-1462852905618-1ky16msvot2ac-630-80.jpg")
+bike_11.user = user_1
+bike_11.save!
+bike_12 = Bike.new(brand: "Batavus", spec: "mountain", description:"Perfect for downhill", city: "Lisbon", address:"Rua Alexandre Herculano 4, Lisboa", price: 55, photo: "http://res.cloudinary.com/dywzzd0as/image/upload/v1519822179/trek7k.jpg")
+bike_12.user = user_2
+bike_12.save!
 
 
