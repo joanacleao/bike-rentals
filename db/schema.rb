0fe49636e23d944284234691b067a1bc11bee6a2
Indiana Franco <indianadfranco@gmail.com>
2018-02-28 12:30:01 +0000

    added geocode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226180541) do
+ActiveRecord::Schema.define(version: 20180228112640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20180226180541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "spec"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
