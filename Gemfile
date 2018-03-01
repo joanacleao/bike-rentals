7e18eec2c98a1c781a277f14bee5462966590080
Alexandre Fontes <alexandrefontes90@gmail.com>
2018-03-01 14:31:41 +0000

    facebook_log_in_first_attempt_DELETE

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.4.3'
 
+gem 'omniauth-facebook'
 gem 'cloudinary'
 gem 'jquery-rails'
 gem 'devise'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+action="https://www.accountkit.com/v1.0/basic/dialog/email_login/"
 
 end
