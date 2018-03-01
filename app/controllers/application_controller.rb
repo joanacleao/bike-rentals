7283fe69702dcfe122e61c48b192380f554eae36
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-03-01 15:35:17 +0000

    Authenticate user on Booking submit and redirect to show/root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
+    protected
+    def after_sign_in_path_for(resource)
+      if !session[:foo].nil?
+        bike_path(session[:foo])
+      else
+        root_path
+      end
+    end
 
 end
