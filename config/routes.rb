0ddafd2d57b55a94c7d2a59d2a22ac4b360e03c7
joanaleao <joanacastroleao@gmail.com>
2018-03-02 13:49:46 +0000

    mybookings as a owner as a user

--- config/routes.rb
+++ config/routes.rb
@@ -18,12 +18,13 @@ Rails.application.routes.draw do
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   get 'bikes/mybikes' , to: 'bikes#mybikes'
+  get 'bikes/mybookings' , to: 'bikes#mybookings'
 
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :bikes do
-    resources :bookings, only: [:new, :create, :show] do
+    resources :bookings, only: [:new, :create, :show, :index] do
       resources :reviews, only: [:new, :create, :show]
     end
   end
